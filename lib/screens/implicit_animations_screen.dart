506df437957e50c1fb42c23402b29fc0c2b039ff
hyejj19@naver.com <hyejj19@naver.com>
2025-03-11 10:32:08 +0900

    #1.1 Implicitly Animated Widgets

--- lib/screens/implicit_animations_screen.dart
+++ lib/screens/implicit_animations_screen.dart
@@ -9,8 +9,42 @@ class ImplicitAnimationsScreen extends StatefulWidget {
 }
 
 class _ImplicitAnimationsScreenState extends State<ImplicitAnimationsScreen> {
+  bool _visible = true;
+
+  void _trigger() {
+    _visible = !_visible;
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedAlign(
+              alignment: _visible ? Alignment.topLeft : Alignment.topRight,
+              duration: Duration(milliseconds: 300),
+              child: AnimatedOpacity(
+                duration: Duration(milliseconds: 300),
+                opacity: _visible ? 1 : 0,
+                child: Container(
+                  width: size.width * 0.8,
+                  height: size.width * 0.8,
+                  color: Colors.amber,
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(onPressed: _trigger, child: Text('go'))
+          ],
+        ),
+      ),
+    );
   }
 }
