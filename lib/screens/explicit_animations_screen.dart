923508b1289ac244e382e82ccbc60162f9a29dd1
hyejj19 <hyejj19@naver.com>
2025-03-13 15:12:46 +0900

    #2.2 Animation values

---
+++ lib/screens/explicit_animations_screen.dart
@@ -0,0 +1,66 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class ExplicitAnimationsScreen extends StatefulWidget {
+  const ExplicitAnimationsScreen({super.key});
+
+  @override
+  State<ExplicitAnimationsScreen> createState() =>
+      _ExplicitAnimationsScreenState();
+}
+
+class _ExplicitAnimationsScreenState extends State<ExplicitAnimationsScreen>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(
+        seconds: 10,
+      ));
+
+  void _play() {
+    _animationController.forward();
+  }
+
+  void _pause() {
+    _animationController.stop();
+  }
+
+  void _rewind() {
+    _animationController.reverse();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Explicit animation'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Text("${_animationController.value}"),
+          ),
+          ElevatedButton(
+            onPressed: _play,
+            child: Text('Play'),
+          ),
+          ElevatedButton(
+            onPressed: _pause,
+            child: Text('pause'),
+          ),
+          ElevatedButton(
+            onPressed: _rewind,
+            child: Text('rewind'),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/menu_screen.dart
+++ lib/screens/menu_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations_masterclass/screens/explicit_animations_screen.dart';
 import 'package:flutter_animations_masterclass/screens/implicit_animation_challenge_screen.dart';
 import 'package:flutter_animations_masterclass/screens/implicit_animations_screen.dart';
 
@@ -40,6 +41,15 @@ class MenuScreen extends StatelessWidget {
                 );
               },
               child: Text('Implicit Animations Challenge'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _goToPage(
+                  context,
+                  ExplicitAnimationsScreen(),
+                );
+              },
+              child: Text('Explicit Animations'),
             ),
           ],
         ),
