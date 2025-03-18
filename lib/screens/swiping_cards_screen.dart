df47e2abd9c1c3de06c3c3d8a15b1ed78c8fa7a1
hyejj19@naver.com <hyejj19@naver.com>
2025-03-18 21:48:52 +0900

    #4.2 Tween Transform

--- lib/screens/swiping_cards_screen.dart
+++ lib/screens/swiping_cards_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class SwipingCardsScreen extends StatefulWidget {
@@ -10,28 +12,33 @@ class SwipingCardsScreen extends StatefulWidget {
 class _SwipingCardsScreenState extends State<SwipingCardsScreen>
     with SingleTickerProviderStateMixin {
   late final size = MediaQuery.of(context).size;
-  late final AnimationController _animationController = AnimationController(
+  late final AnimationController _position = AnimationController(
     vsync: this,
-    duration: Duration(milliseconds: 300),
+    duration: Duration(seconds: 1),
     lowerBound: size.width * -1,
     upperBound: size.width,
     value: 0.0,
   );
 
+  late final Tween<double> _rotation = Tween(
+    begin: -15,
+    end: 15,
+  );
+
   void _onHorizontalDragUpdate(DragUpdateDetails detail) {
-    _animationController.value += detail.delta.dx;
+    _position.value += detail.delta.dx;
   }
 
   void _onHorizontalDragEnd(DragEndDetails detail) {
-    _animationController.animateTo(
+    _position.animateTo(
       0,
-      curve: Curves.bounceOut,
+      curve: Curves.easeInOut,
     );
   }
 
   @override
   void dispose() {
-    _animationController.dispose();
+    _position.dispose();
     super.dispose();
   }
 
@@ -42,29 +49,41 @@ class _SwipingCardsScreenState extends State<SwipingCardsScreen>
         title: Text('Swiping Cards'),
       ),
       body: AnimatedBuilder(
-        animation: _animationController,
-        builder: (context, child) => Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: GestureDetector(
-                onHorizontalDragUpdate: _onHorizontalDragUpdate,
-                onHorizontalDragEnd: _onHorizontalDragEnd,
-                child: Transform.translate(
-                  offset: Offset(_animationController.value, 0),
-                  child: Material(
-                    elevation: 10,
-                    color: Colors.red.shade100,
-                    child: SizedBox(
-                      width: size.width * 0.8,
-                      height: size.height * 0.5,
+        animation: _position,
+        builder: (context, child) {
+          // position 값에 따라서 회전 각도를 계산
+          // size.width/2 => 전체 너비의 절반을 더해 현재 position 의 중앙을 기준점으로 함
+          // 이를 다시 size.width 로 나누어서 0~1 사이의 비율로 만듦 (어떤 값을 전체 크기로 나누면 비율이 된다는 것...!)
+          final angle = _rotation
+              .transform((_position.value + size.width / 2) / size.width);
+          return Stack(
+            children: [
+              Align(
+                alignment: Alignment.topCenter,
+                child: GestureDetector(
+                  onHorizontalDragUpdate: _onHorizontalDragUpdate,
+                  onHorizontalDragEnd: _onHorizontalDragEnd,
+                  child: Transform.translate(
+                    offset: Offset(_position.value, 0),
+                    child: Transform.rotate(
+                      angle: angle *
+                          pi /
+                          180, // 각도를 라디안으로 변환, angle 값에서 호의 개념으로 전환..
+                      child: Material(
+                        elevation: 10,
+                        color: Colors.red.shade100,
+                        child: SizedBox(
+                          width: size.width * 0.8,
+                          height: size.height * 0.5,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
-            ),
-          ],
-        ),
+            ],
+          );
+        },
       ),
     );
   }
