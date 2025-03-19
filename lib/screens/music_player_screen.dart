4ffb0b2e41b73b5d6a3ef187b159b0007ac82741
hyejj19 <hyejj19@naver.com>
2025-03-19 17:59:21 +0900

    #5.1 AnimatedSwitcher

--- lib/screens/music_player_screen.dart
+++ lib/screens/music_player_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class MusicPlayerScreen extends StatefulWidget {
@@ -12,13 +14,44 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
     viewportFraction: 0.8,
   );
 
+  int _currentPage = 0;
+
+  void _onPageChanged(int newPage) {
+    setState(() {
+      _currentPage = newPage;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 300),
+            child: Container(
+              key: ValueKey(_currentPage),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/covers/${_currentPage + 1}.jpg"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: 15,
+                  sigmaY: 15,
+                ),
+                child: Container(
+                  color: Colors.black38,
+                ),
+              ),
+            ),
+          ),
           PageView.builder(
             controller: _pageController,
+            onPageChanged: _onPageChanged,
             itemCount: 5,
             scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
@@ -28,23 +61,34 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                   Container(
                     height: 350,
                     decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image:
-                                AssetImage("assets/covers/${index + 1}.jpg"))),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black38,
+                          blurRadius: 10,
+                          spreadRadius: 2,
+                          offset: Offset(0, 9),
+                        )
+                      ],
+                      borderRadius: BorderRadius.circular(20),
+                      image: DecorationImage(
+                        image: AssetImage("assets/covers/${index + 1}.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
                   SizedBox(
                     height: 35,
                   ),
                   Text(
                     'Interstella',
-                    style: TextStyle(fontSize: 26),
+                    style: TextStyle(fontSize: 26, color: Colors.white),
                   ),
                   SizedBox(
                     height: 10,
                   ),
                   Text(
                     'Hans zimmer',
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(fontSize: 18, color: Colors.white),
                   ),
                 ],
               );
